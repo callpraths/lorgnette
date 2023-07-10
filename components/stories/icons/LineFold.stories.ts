b6907f621acfb018c52a8de74b911184c664193a
Prathmesh Prabhu <callpraths@gmail.com>
2023-07-09 23:56:09 -0700

    Add button story

--- components/stories/icons/LineFold.stories.ts
+++ components/stories/icons/LineFold.stories.ts
@@ -1,5 +1,6 @@
-import { html } from 'lit';
+import { LitElement, html } from 'lit';
 import '../../src/define.js';
+import { property } from 'lit/decorators.js';
 import { withCard } from '../utils.js';
 
 export default {
@@ -28,4 +29,38 @@ export const FoldedHighlight = () =>
   withCard(
     html`Lorem ipsum dolor sit amet, consectetur adipiscing elit
       <lvi-icon-line-fold expanded highlighted></lvi-icon-line-fold>`
+  );
+
+class LineFoldButton extends LitElement {
+  @property({ type: Boolean })
+  highlighted: boolean = false;
+
+  @property({ type: Boolean })
+  expanded: boolean = false;
+
+  render() {
+    return html`<sl-button variant="text" @click=${this.toggle}>
+      <lvi-icon-line-fold
+        ?highlighted=${this.highlighted}
+        ?expanded=${this.expanded}
+      ></lvi-icon-line-fold>
+    </sl-button>`;
+  }
+
+  private toggle() {
+    this.expanded = !this.expanded;
+  }
+}
+
+window.customElements.define('sbx-line-fold-button', LineFoldButton);
+
+export const Button = () =>
+  withCard(
+    html`Lorem ipsum dolor sit amet, consectetur adipiscing elit
+      <sbx-line-fold-button></sbx-line-fold-button>`
+  );
+export const HighlightedButton = () =>
+  withCard(
+    html`Lorem ipsum dolor sit amet, consectetur adipiscing elit
+      <sbx-line-fold-button highlighted></sbx-line-fold-button>`
   );
